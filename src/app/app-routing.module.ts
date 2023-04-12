35f0c6e9e78551789715bb531b962fc686ef5701
Emanuel Jeisom <emjeisom.dev@gmail.com>
2023-04-11 22:23:37 -0300

    adding new rotes on sidenav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductsComponent } from './products/products.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { CoupensComponent } from './coupens/coupens.component';
+import { PagesComponent } from './pages/pages.component';
+import { MediasComponent } from './medias/medias.component';
+import { SettingsComponent } from './settings/settings.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
-  {path: 'dashboard', component: DashboardComponent}
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'statistics', component: StatisticsComponent},
+  {path: 'coupens', component: CoupensComponent},
+  {path: 'pages', component: PagesComponent},
+  {path: 'media', component: MediasComponent},
+  {path: 'settings', component: SettingsComponent}
 ];
 
 @NgModule({
