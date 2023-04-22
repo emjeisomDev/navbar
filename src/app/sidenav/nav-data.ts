6b612a1daeb10b5e5908ef45a1737d625771dcd0
emjeisomDev <emjeisom.dev@gmail.com>
2023-04-22 11:46:33 -0300

    Configured second submenu sublevel

--- src/app/sidenav/nav-data.ts
+++ src/app/sidenav/nav-data.ts
@@ -12,7 +12,27 @@ export const navbarData:INavbarData[] = [
         items: [
             {
                 routeLink: 'products/level1.1',
-                label: 'Level 1.1'
+                label: 'Level 1.1',
+                items: [
+                    {
+                        routeLink: 'products/level2.1',
+                        label: 'Level 2.1',
+                    },
+                    {
+                        routeLink: 'products/level2.2',
+                        label: 'Level 2.2',
+                        items: [
+                            {
+                                routeLink: 'products/level3.1',
+                                label: 'Level 3.1',
+                            },
+                            {
+                                routeLink: 'products/level3.2',
+                                label: 'Level 3.2',
+                            },
+                        ]
+                    },
+                ]
             },
             {
                 routeLink: 'products/level1.2',
@@ -53,7 +73,18 @@ export const navbarData:INavbarData[] = [
     {
         routeLink: 'settings',
         icon: 'fal fa-cog',
-        label: 'Settings'
+        label: 'Settings',
+        expanded: true,
+        items: [
+            {
+                routeLink: 'settings/profile',
+                label: 'Profile'
+            },
+            {
+                routeLink: 'settings/customize',
+                label: 'Customize'
+            },
+        ]
     },
 
 ];
